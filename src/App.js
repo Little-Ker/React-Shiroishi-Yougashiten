52ea9baa8b8257096127d0814f37db0beb9f4cbe
Vivi <vivichi@yile.com.tw>
2023-11-03 18:13:53 +0800

    [新增] git pages 部屬

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {
-  BrowserRouter as Router, Route, Routes 
+  HashRouter as Router, Route, Routes 
 } from 'react-router-dom'
 import './App.css'
 // import Navbar from 'component/navbar/Navbar'
